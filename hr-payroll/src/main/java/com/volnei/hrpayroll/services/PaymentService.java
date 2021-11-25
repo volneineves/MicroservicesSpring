d8c0bf3365c0a935bbf9f856246849cb4cee276c
Volnei Neves <82004090+volneineves@users.noreply.github.com>
2021-11-25 10:11:16 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/volnei/hrpayroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.volnei.hrpayroll.services;
+
+import com.volnei.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days){
+        return new Payment("Bob", 200.0, days);
+    }
+}
