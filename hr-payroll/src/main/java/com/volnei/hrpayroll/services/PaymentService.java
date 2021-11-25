e0367bdc12cc75f761be186dbe19a7dd30e36d99
Volnei Neves <82004090+volneineves@users.noreply.github.com>
2021-11-25 10:46:28 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/volnei/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/volnei/hrpayroll/services/PaymentService.java
@@ -1,12 +1,30 @@
 package com.volnei.hrpayroll.services;
 
 import com.volnei.hrpayroll.entities.Payment;
+import com.volnei.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.0, days);
+
+        Map<String, String> uriVariable = new HashMap<>();
+        uriVariable.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
