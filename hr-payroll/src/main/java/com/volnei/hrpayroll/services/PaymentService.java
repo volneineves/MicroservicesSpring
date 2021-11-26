b71a1bdcdc323b7e3947fedb482a970f09814321
Volnei Neves <82004090+volneineves@users.noreply.github.com>
2021-11-26 13:49:58 -0300

    Configuring Zull api gateway

--- hr-payroll/src/main/java/com/volnei/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/volnei/hrpayroll/services/PaymentService.java
@@ -18,10 +18,9 @@ public class PaymentService {
     @Autowired
     private WorkerFeignClient workerFeignClient;
 
-    public Payment getPayment(long workerId, int days){
+    public Payment getPayment(long workerId, int days) {
 
         Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
-
 }
--- hr-worker/src/main/java/com/volnei/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/volnei/hrworker/resources/WorkerResource.java
@@ -27,10 +27,25 @@ public class WorkerResource {
     @Autowired
     private WorkerRepository repository;
 
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll() {
+        List<Worker> list = repository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
+		/*
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		*/
+
         logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
