5c0793116cb6b39dafc268803fc4c486d9a16f62
Volnei Neves <82004090+volneineves@users.noreply.github.com>
2021-11-26 17:20:01 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/volnei/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/volnei/hrworker/resources/WorkerResource.java
@@ -21,11 +21,20 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerRepository repository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
