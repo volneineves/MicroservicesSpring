5fdbcece07b0c9c11497d00269dafea33a30d64a
Volnei Neves <82004090+volneineves@users.noreply.github.com>
2021-11-26 11:41:30 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/com/volnei/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volnei.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
