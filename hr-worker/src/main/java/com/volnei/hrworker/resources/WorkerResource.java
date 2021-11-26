be70c6743c0c4c5657964c3ce88f21bfbf9dd809
Volnei Neves <82004090+volneineves@users.noreply.github.com>
2021-11-26 18:02:48 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/com/volnei/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/volnei/hrworker/resources/WorkerResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.volnei.hrworker.entities.Worker;
 import com.volnei.hrworker.repositories.WorkerRepository;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
@@ -45,11 +47,11 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
