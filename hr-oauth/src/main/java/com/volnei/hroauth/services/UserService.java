3d44ab46450cfde9e744f98c3a4801bee5e0e330
Volnei Neves <82004090+volneineves@users.noreply.github.com>
2021-11-28 23:05:48 -0300

    Login and JWT token

--- hr-oauth/src/main/java/com/volnei/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/volnei/hroauth/services/UserService.java
@@ -5,12 +5,15 @@ import com.volnei.hroauth.feignclients.UserFeignCLient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
-   private static Logger logger = LoggerFactory.getLogger(UserService.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserFeignCLient userFeignCLient;
@@ -23,6 +26,19 @@ public class UserService {
         }
 
         logger.info("Email found: " + email);
+
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignCLient.findByEmail(username).getBody();
+        if (user == null) {
+            logger.error("Email not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        logger.info("Email found: " + username);
 
         return user;
     }
